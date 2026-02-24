3295c5489fda934b7f711925a228c2381d292b54
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-02-24 08:30:38 +0530

    Customer CRUD implemented

--- shopify/src/main/java/com/ecom_project/shopify/controller/CustomerController.java
+++ shopify/src/main/java/com/ecom_project/shopify/controller/CustomerController.java
@@ -1,11 +1,40 @@
 package com.ecom_project.shopify.controller;
 
+import com.ecom_project.shopify.model.Customer;
 import com.ecom_project.shopify.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/customer")
+import java.util.List;
+import java.util.UUID;
+
+@RestController
 public class CustomerController {
     @Autowired
     CustomerService customerService;
+
+    //getAll
+    @GetMapping("/customer/getAll")
+    public List<Customer> getCustomers(){
+       return customerService.getAllCustomers();
+    }
+
+    //get by id
+    @GetMapping("/customer/getById/{id}")
+    public Customer getCustomerById(@PathVariable UUID id){
+        return customerService.getCustomerById(id);
+    }
+
+    // put
+    @PostMapping("/customer/addCustomer")
+    public void addCustomer(@RequestBody Customer customer){
+        customerService.addCustomer(customer);
+    }
+
+    // delete
+    @DeleteMapping("/customer/deleteById/{id}")
+    public void deleteCustomer(@PathVariable UUID id){
+        customerService.deleteCustomer(id);
+    }
+
 }
--- shopify/src/main/java/com/ecom_project/shopify/service/CustomerService.java
+++ shopify/src/main/java/com/ecom_project/shopify/service/CustomerService.java
@@ -1,11 +1,32 @@
 package com.ecom_project.shopify.service;
 
+import com.ecom_project.shopify.model.Customer;
 import com.ecom_project.shopify.repository.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class CustomerService {
     @Autowired
     CustomerRepo customerRepo;
+
+    public List<Customer> getAllCustomers() {
+       return customerRepo.findAll();
+    }
+
+    public Customer getCustomerById(UUID id) {
+        return (Customer) customerRepo.findById(id).orElse(null);
+    }
+
+    public void addCustomer(Customer customer) {
+        customerRepo.save(customer);
+    }
+
+
+    public void deleteCustomer(UUID id) {
+        customerRepo.deleteById(id);
+    }
 }
