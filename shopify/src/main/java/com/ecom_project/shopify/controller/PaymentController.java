a790b6745d725b83dc9726bd42222f69ea9a64e9
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-02-24 08:32:32 +0530

    Payment CRUD implemented

--- shopify/src/main/java/com/ecom_project/shopify/controller/PaymentController.java
+++ shopify/src/main/java/com/ecom_project/shopify/controller/PaymentController.java
@@ -1,12 +1,40 @@
 package com.ecom_project.shopify.controller;
 
+import com.ecom_project.shopify.model.Payment;
 import com.ecom_project.shopify.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
-@RestController("/payment")
+@RestController
 public class PaymentController {
     @Autowired
     PaymentService paymentService;
+
+    //getAll
+    @GetMapping("/payment/getAll")
+    public List<Payment> getPayments(){
+        return paymentService.getAllPayments();
+    }
+
+    //get by id
+    @GetMapping("/payment/getById/{id}")
+    public Payment getPaymentById(@PathVariable Integer id){
+        return paymentService.getPaymentById(id);
+    }
+
+    // put
+    @PostMapping("/payment/addPayment")
+    public void addPayment(@RequestBody Payment payment){
+        paymentService.addPayment(payment);
+    }
+
+
+    // delete
+    @DeleteMapping("/payment/delete/{id}")
+    public void deletePaymentById(@PathVariable Integer id){
+        paymentService.deletePayment(id);
+    }
 }
--- shopify/src/main/java/com/ecom_project/shopify/service/PaymentService.java
+++ shopify/src/main/java/com/ecom_project/shopify/service/PaymentService.java
@@ -1,11 +1,30 @@
 package com.ecom_project.shopify.service;
 
+import com.ecom_project.shopify.model.Payment;
 import com.ecom_project.shopify.repository.PaymentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PaymentService {
     @Autowired
     PaymentRepo paymentRepo;
+
+    public List<Payment> getAllPayments() {
+        return paymentRepo.findAll();
+    }
+
+    public Payment getPaymentById(Integer id) {
+        return paymentRepo.findById(id).orElse(null);
+    }
+
+    public void addPayment(Payment payment) {
+        paymentRepo.save(payment);
+    }
+
+    public void deletePayment(Integer id) {
+        paymentRepo.deleteById(id);
+    }
 }
