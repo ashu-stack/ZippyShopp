ad617eab64122d1485fcf4e439f990f488a885c7
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-02-26 18:49:25 +0530

    Added make order functionality

--- shopify/src/main/java/com/ecom_project/shopify/service/CustomerService.java
+++ shopify/src/main/java/com/ecom_project/shopify/service/CustomerService.java
@@ -1,10 +1,17 @@
 package com.ecom_project.shopify.service;
 
 import com.ecom_project.shopify.model.Customer;
+import com.ecom_project.shopify.model.Orders;
+import com.ecom_project.shopify.model.Payment;
+import com.ecom_project.shopify.model.Product;
 import com.ecom_project.shopify.repository.CustomerRepo;
+import com.ecom_project.shopify.repository.OrderRepo;
+import com.ecom_project.shopify.repository.PaymentRepo;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,6 +19,23 @@ import java.util.UUID;
 public class CustomerService {
     @Autowired
     CustomerRepo customerRepo;
+    OrderRepo orderRepo;
+    PaymentRepo paymentRepo;
+
+    @Transactional
+    public void makeOrder(List<Product> list, UUID custId, String modeOfPayment){
+
+        int amount = 0;
+        for(Product product : list){
+            amount = amount + product.getPrice();
+        }
+        Customer customer = (Customer) customerRepo.findById(custId).orElse(null);
+        Orders orders = Orders.builder().custId(custId).amount(amount).orderDate(LocalDate.now()).customer(customer).build();
+        orderRepo.save(orders);
+
+        Payment payment = Payment.builder().type(modeOfPayment).customer(customer).amount(amount).build();
+        paymentRepo.save(payment);
+    }
 
     public List<Customer> getAllCustomers() {
        return customerRepo.findAll();
