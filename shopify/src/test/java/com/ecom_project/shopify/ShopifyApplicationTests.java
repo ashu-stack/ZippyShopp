8fd5b54fde230b17c936068a1de4ae74df9b0b55
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-01 12:24:46 +0530

    Test for add and remove from cart

--- shopify/src/test/java/com/ecom_project/shopify/ShopifyApplicationTests.java
+++ shopify/src/test/java/com/ecom_project/shopify/ShopifyApplicationTests.java
@@ -1,7 +1,9 @@
 package com.ecom_project.shopify;
 
+import com.ecom_project.shopify.model.Cart;
 import com.ecom_project.shopify.model.Orders;
 import com.ecom_project.shopify.model.Product;
+import com.ecom_project.shopify.service.CartService;
 import com.ecom_project.shopify.service.CustomerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,10 @@ import java.util.UUID;
 class ShopifyApplicationTests {
 	@Autowired
 	CustomerService customerService;
+
+	@Autowired
+	CartService cartService;
+
 	@Test
 	void checkMakePaymentMethod() {
 		Product product = Product.builder().name("Laptop").price(50000).available(true).build();
@@ -25,9 +31,26 @@ class ShopifyApplicationTests {
 		UUID custId = UUID.fromString("550e8400-e29b-41d4-a716-446655440000");
 		String paymentMode = "COD";
 
-		 Orders orders = customerService.makeOrder(list,custId,paymentMode);
-		System.out.println(orders);
+		// Orders orders = customerService.makeOrder(list,custId,paymentMode);
+		//System.out.println(orders);
 
+	}
+
+	@Test
+	void testAddToCart(){
+		Product product = Product.builder().name("Laptop").price(50000).available(true).build();
+		UUID custId = UUID.fromString("550e8400-e29b-41d4-a716-446655440000");
+		Cart cart = cartService.addToCart(product,custId);
+		System.out.println(cart);
+
+	}
+
+	@Test
+	void testRemoveFromCart(){
+		Product product = Product.builder().name("Laptop").price(50000).available(true).build();
+		UUID custId = UUID.fromString("550e8400-e29b-41d4-a716-446655440000");
+		Cart cart = cartService.removeFromCart(product,custId);
+		System.out.println(cart);
 	}
 
 }
