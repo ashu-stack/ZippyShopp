26fec31fb225e5f18f0008ed484c8a8dfc54f5a8
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-06 21:09:08 +0530

    Security configs modified

--- shopify/src/main/java/com/ecom_project/shopify/config/SecurityConfig.java
+++ shopify/src/main/java/com/ecom_project/shopify/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +19,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(request -> request.anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults()).build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+         
     }
 }
