b27132ad2a3388ccb788efdabd587bbf0fceae6a
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-07 15:12:36 +0530

    Injected user beans

--- shopify/src/main/java/com/ecom_project/shopify/config/SecurityConfig.java
+++ shopify/src/main/java/com/ecom_project/shopify/config/SecurityConfig.java
@@ -1,16 +1,25 @@
 package com.ecom_project.shopify.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    public final PasswordEncoder passwordEncoder;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -23,6 +32,10 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService(){
-         
+        UserDetails user1 = User.withUsername("user").password("pass").roles("USER").build();
+        UserDetails user2 = User.withUsername("admin").password("passAd").roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user1,user2);
+
     }
 }
