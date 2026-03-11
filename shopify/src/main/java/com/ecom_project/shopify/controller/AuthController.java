8f7d1bf92821cc42927654d46a7f666d49125480
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-11 10:01:22 +0530

    Auth controller created

---
+++ shopify/src/main/java/com/ecom_project/shopify/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.ecom_project.shopify.controller;
+
+import com.ecom_project.shopify.dto.LoginRequestDto;
+import com.ecom_project.shopify.dto.LoginResponseDto;
+import com.ecom_project.shopify.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(LoginRequestDto loginRequestDto){
+       LoginResponseDto loginResponseDto = authService.login(loginRequestDto);
+       return new ResponseEntity<>(loginResponseDto, HttpStatus.OK);
+    }
+}
