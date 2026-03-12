8f30610f16bf3be09cfd9bde12fd945ad6a28258
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-12 09:30:05 +0530

    SignupRequestDto created

---
+++ shopify/src/main/java/com/ecom_project/shopify/dto/SignupRequestDto.java
@@ -0,0 +1,14 @@
+package com.ecom_project.shopify.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignupRequestDto {
+
+    private Long id;
+    private String username;
+}
