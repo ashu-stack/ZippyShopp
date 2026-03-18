a7cc67005176ccfb93ee5eb41a2d38285623939a
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-18 10:23:25 +0530

    functions to get provider id and type added

--- shopify/src/main/java/com/ecom_project/shopify/util/AuthUtil.java
+++ shopify/src/main/java/com/ecom_project/shopify/util/AuthUtil.java
@@ -1,11 +1,13 @@
 package com.ecom_project.shopify.util;
 
+import com.ecom_project.shopify.model.AuthProviderType;
 import com.ecom_project.shopify.model.Customer;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -42,4 +44,29 @@ public class AuthUtil {
 
         return claims.getSubject();
     }
+
+
+    public AuthProviderType providerType(String registerId){
+        return switch (registerId.toLowerCase()) {
+            case "google" -> AuthProviderType.GOOGLE;
+            case "github" -> AuthProviderType.GITHUB;
+            default -> throw new IllegalArgumentException("Unsupported auth provider: {}" + registerId);
+        };
+    }
+
+    public String providerId(OAuth2User oAuth2User, String registrationId) {
+
+        String providerId = switch (registrationId.toLowerCase()) {
+            case "google" -> oAuth2User.getAttribute("sub");
+            case "github" -> oAuth2User.getAttribute("id").toString();
+            default -> throw new IllegalArgumentException("Unsupported provider: " + registrationId);
+        };
+
+        if (providerId == null || providerId.isBlank()) {
+            throw new IllegalArgumentException("provider cant be determined");
+        }
+
+        return providerId;
+    }
+
 }
